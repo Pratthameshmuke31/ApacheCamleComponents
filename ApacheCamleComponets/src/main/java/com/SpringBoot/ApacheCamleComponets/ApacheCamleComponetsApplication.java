4c05f60dec84c1c1bea513ef0b714daa8f4fd73b
Prathamesh.m <Prathamesh.m@evolvussolutions.com>
2023-02-21 18:32:18 +0530

    ApacheCamleComponets

---
+++ ApacheCamleComponets/src/main/java/com/SpringBoot/ApacheCamleComponets/ApacheCamleComponetsApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ApacheCamleComponets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamleComponetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamleComponetsApplication.class, args);
+	}
+
+}
---
+++ ApacheCamleComponets/src/test/java/com/SpringBoot/ApacheCamleComponets/ApacheCamleComponetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ApacheCamleComponets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamleComponetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
